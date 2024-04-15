066b133618e9f6e969f9f019a7f317cd912d2c44
Driftay <driftayllc@gmail.com>
2024-04-15 13:56:09 -0400

    Implemented enhanced documentation for readability and maintainability.

--- src/main/java/org/saberdev/tomlizer/CustomTomlFile.java
+++ src/main/java/org/saberdev/tomlizer/CustomTomlFile.java
@@ -15,17 +15,35 @@ public class CustomTomlFile {
     private final Toml toml;
     private Map<String, Object> cachedObjects = new HashMap<>();
 
+    /**
+     * Constructor for creating a new instance of CustomTomlFile.
+     *
+     * @param file The file to be read and written.
+     */
     public CustomTomlFile(File file) {
         this.file = file;
         this.toml = new Toml().read(file);
     }
 
+    /**
+     * Saves the current TOML data to the specified file.
+     *
+     * @throws IOException If an error occurs while writing to the file.
+     */
     public void saveToFile() throws IOException {
         try (Writer writer = new FileWriter(file)) {
             new TomlWriter().write(toml, writer);
         }
     }
 
+    /**
+     * Fetches the value of a specified key from the TOML data.
+     *
+     * @param key   The key to fetch the value for.
+     * @param clazz The expected type of the value.
+     * @return The value of the specified key, cast to the specified type.
+     * @throws IllegalArgumentException If the key is not found or the value is not of the expected type.
+     */
     public <T> T fetch(String key, Class<T> clazz) {
         if (cachedObjects.containsKey(key) && cachedObjects.get(key).getClass().equals(clazz)) {
             return clazz.cast(cachedObjects.get(key));
@@ -53,6 +71,12 @@ public class CustomTomlFile {
         return clazz.cast(result);
     }
 
+    /**
+     * Checks if the specified key exists in the TOML data.
+     *
+     * @param key The key to check for.
+     * @return True if the key exists, false otherwise.
+     */
     public boolean containsKey(String key) {
         return cachedObjects.containsKey(key) || toml.contains(key);
     }
--- src/main/java/org/saberdev/tomlizer/CustomTomlFileBuilder.java
+++ src/main/java/org/saberdev/tomlizer/CustomTomlFileBuilder.java
@@ -9,11 +9,31 @@ import java.nio.file.StandardCopyOption;
 import java.util.Objects;
 
 public class CustomTomlFileBuilder {
+    /**
+     * The path to the data folder where the custom TOML file will be created.
+     */
     private final Path dataFolder;
+
+    /**
+     * The name of the custom TOML file, without the ".toml" extension.
+     */
     private final String fileName;
+
+    /**
+     * A flag indicating whether the custom TOML file should be loaded from the project.
+     */
     private final boolean loadFromProject;
+
+    /**
+     * The folder name where the custom TOML file will be created, if it's loaded from the project.
+     */
     private final String inFolder;
 
+    /**
+     * Constructs a new instance of the {@code CustomTomlFileBuilder} class.
+     *
+     * @param builder the builder instance
+     */
     public CustomTomlFileBuilder(Builder builder) {
         this.dataFolder = Objects.requireNonNull(builder.dataFolder);
         this.fileName = Objects.requireNonNull(builder.fileName) + ".toml";
@@ -21,40 +41,89 @@ public class CustomTomlFileBuilder {
         this.inFolder = builder.inFolder != null ? builder.inFolder : "";
     }
 
+    /**
+     * A static nested class representing a builder for creating a custom TOML file.
+     */
     public static class Builder {
+
+        /**
+         * The path to the data folder where the custom TOML file will be created.
+         */
         private final Path dataFolder;
+
+        /**
+         * The name of the custom TOML file, without the ".toml" extension.
+         */
         private String fileName;
+
+        /**
+         * A flag indicating whether the custom TOML file should be loaded from the project.
+         */
         private boolean loadFromProject = false;
+
+        /**
+         * The folder name where the custom TOML file will be created, if it's loaded from the project.
+         */
         private String inFolder;
 
+        /**
+         * Constructs a new instance of the {@code Builder} class.
+         *
+         * @param dataFolder the path to the data folder
+         */
         public Builder(Path dataFolder) {
             this.dataFolder = Objects.requireNonNull(dataFolder);
         }
 
+        /**
+         * Sets the name of the custom TOML file.
+         *
+         * @param fileName the name of the custom TOML file
+         * @return the builder instance
+         */
         public Builder fileName(String fileName) {
             this.fileName = fileName;
             return this;
         }
 
+        /**
+         * Sets the flag indicating whether the custom TOML file should be loaded from the project.
+         *
+         * @return the builder instance
+         */
         public Builder loadFromProject() {
             this.loadFromProject = true;
             return this;
         }
 
+        /**
+         * Sets the folder name where the custom TOML file will be created, if it's loaded from the project.
+         *
+         * @param folderName the folder name
+         * @return the builder instance
+         */
         public Builder inFolder(String folderName) {
             this.inFolder = folderName;
             return this;
         }
 
-        public CustomTomlFile build() {
-            try {
-                return new CustomTomlFileBuilder(this).buildInternal();
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to build custom TOML file", e);
-            }
+        /**
+         * Builds the custom TOML file builder and returns the {@code CustomTomlFile} instance.
+         *
+         * @return the {@code CustomTomlFile} instance
+         * @throws IOException if an error occurs while building the custom TOML file
+         */
+        public CustomTomlFile build() throws IOException {
+            return new CustomTomlFileBuilder(this).buildInternal();
         }
     }
 
+    /**
+     * Builds the custom TOML file.
+     *
+     * @return the {@code CustomTomlFile} instance
+     * @throws IOException if an error occurs while building the custom TOML file
+     */
     private CustomTomlFile buildInternal() throws IOException {
         Path filePath = dataFolder.resolve(fileName);
         if (!Files.exists(filePath)) {
@@ -67,11 +136,23 @@ public class CustomTomlFileBuilder {
         return new CustomTomlFile(filePath.toFile());
     }
 
+    /**
+     * Creates a new file in the specified directory.
+     *
+     * @param filePath the path to the file
+     * @throws IOException if an error occurs while creating the file
+     */
     private void createNewFile(Path filePath) throws IOException {
         Files.createDirectories(filePath.getParent());
         Files.createFile(filePath);
     }
 
+    /**
+     * Extracts a resource from the classpath and saves it to the specified file path.
+     *
+     * @param filePath the path to the file
+     * @throws IOException if an error occurs while extracting the resource
+     */
     private void extractResource(Path filePath) throws IOException {
         String resourcePath = inFolder.isEmpty() ? fileName : inFolder + "/" + fileName;
         try (InputStream is = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
